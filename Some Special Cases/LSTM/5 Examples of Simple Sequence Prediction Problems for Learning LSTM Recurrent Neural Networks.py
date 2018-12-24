6f5ebee49188ec8e76a9917695bb9fef575e6d40
zz <2283942163@qq.com>
2018-12-25 07:54:42 +0800

    null

---
+++ Some Special Cases/LSTM/5 Examples of Simple Sequence Prediction Problems for Learning LSTM Recurrent Neural Networks.py
@@ -0,0 +1,24 @@
+
+# https://machinelearningmastery.com/sequence-prediction-problems-learning-lstm-recurrent-neural-networks/
+from numpy import array
+
+# def generate_sequence(length = 10):
+#     return array([i/float(length) for i in range(length)])
+#
+# print(generate_sequence())
+
+
+def generate_sequence(length=5):
+    return [i for i in range(length)]
+
+
+# sequence 1
+seq1 = generate_sequence()
+seq1[0] = seq1[-1] = seq1[-2]
+print(seq1)
+# sequence 2
+seq1 = generate_sequence()
+seq1[0] = seq1[-1]
+print(seq1)
+
+
